520a9a157f251db504226d8aa3f5f6eb4c2a7603
aportraitofjoyce <aportraitofjoyce@gmail.com>
2021-10-15 19:02:37 +0300

    Test branch

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,15 +1,19 @@
-import React from 'react'
+import React, {FC} from 'react'
 import {AppRouter} from './AppRouter'
 import {HashRouter} from 'react-router-dom'
 import {Header} from '../components/Header/Header'
 import {Provider} from 'react-redux'
 import {store} from '../store/store'
 
-export const App = () => (
-    <HashRouter>
-        <Provider store={store}>
-            <Header/>
-            <AppRouter/>
-        </Provider>
-    </HashRouter>
-)
+// TEST
+
+export const App: FC = () => {
+    return (
+        <HashRouter>
+            <Provider store={store}>
+                <Header/>
+                <AppRouter/>
+            </Provider>
+        </HashRouter>
+    )
+}
