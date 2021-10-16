7a574d3328bc3300364fd371b5af771f8c661e7f
It-Shu <it.shustov@gmail.com>
2021-10-16 15:44:05 +0300

    Login form

--- src/pages/Login/Login.tsx
+++ src/pages/Login/Login.tsx
@@ -1,9 +1,34 @@
 import React, {FC} from 'react'
+import {Input} from "../../components/UI/Input/Input";
+import {Button} from "../../components/UI/Button/Button";
+import {Checkbox} from "../../components/UI/Checkbox/Checkbox";
 
 export const Login: FC = () => {
     return (
         <div>
-            <h1>Login</h1>
+            <h1>Sign In</h1>
+            <form action="">
+                <label htmlFor="loginEmail">Email</label>
+                <Input id={'loginEmail'} type={"email"}/>
+
+                <label htmlFor="loginPassword">Password</label>
+                <Input id={'loginPassword'} type={"password"}/>
+
+                <div>
+                    <p>Forgot Password</p>
+                </div>
+
+                <Checkbox>Remember me</Checkbox>
+                <Button type={"submit"} title={'Login'}/>
+                <div>
+                    <a>Don’t have an account?</a>
+                </div>
+                <div>
+                    <a>Sign Up</a>
+                </div>
+
+            </form>
+
         </div>
     )
 }
