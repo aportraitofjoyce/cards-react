f1faf252903df2dbf4981cc5e0db85753730b08f
aportraitofjoyce <aportraitofjoyce@gmail.com>
2021-10-16 15:43:59 +0300

    Register page JSX

--- src/pages/Register/Register.tsx
+++ src/pages/Register/Register.tsx
@@ -1,9 +1,28 @@
 import React, {FC} from 'react'
+import {Input} from '../../components/UI/Input/Input'
+import {Button} from '../../components/UI/Button/Button'
 
 export const Register: FC = () => {
+    const onSubmit = () => {
+        alert('Register')
+    }
+
     return (
         <div>
             <h1>Register</h1>
+            <form onSubmit={onSubmit}>
+                <label htmlFor={'registerEmail'}>Email</label>
+                <Input id={'registerEmail'} type={'email'}/>
+
+                <label htmlFor={'registerPassword'}>Password</label>
+                <Input id={'registerPassword'} type={'password'}/>
+
+                <label htmlFor={'registerConfirmPassword'}>Password</label>
+                <Input id={'registerConfirmPassword'} type={'password'}/>
+
+                <Button>Cancel</Button>
+                <Button type={'submit'}>Register</Button>
+            </form>
         </div>
     )
 }
