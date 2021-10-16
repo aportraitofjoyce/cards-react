678169e5272e00d9e11842bef7bb3807dcd150be
It-Shu <it.shustov@gmail.com>
2021-10-16 16:28:37 +0300

    Login form v2

--- src/pages/Login/Login.tsx
+++ src/pages/Login/Login.tsx
@@ -2,30 +2,39 @@ import React, {FC} from 'react'
 import {Input} from "../../components/UI/Input/Input";
 import {Button} from "../../components/UI/Button/Button";
 import {Checkbox} from "../../components/UI/Checkbox/Checkbox";
+import {NavLink} from "react-router-dom";
+
 
 export const Login: FC = () => {
+    const onSubmit = () => {
+        alert('Login')
+    }
+
     return (
         <div>
             <h1>Sign In</h1>
-            <form action="">
+            <form onSubmit={onSubmit}>
                 <label htmlFor="loginEmail">Email</label>
                 <Input id={'loginEmail'} type={"email"}/>
 
                 <label htmlFor="loginPassword">Password</label>
                 <Input id={'loginPassword'} type={"password"}/>
 
-                <div>
+                <NavLink to={'/password-recovery'}>
                     <p>Forgot Password</p>
-                </div>
+                </NavLink>
 
                 <Checkbox>Remember me</Checkbox>
-                <Button type={"submit"} title={'Login'}/>
+
+                <Button type={"submit"}>Login</Button>
+
                 <div>
-                    <a>Don’t have an account?</a>
+                    <p>Don’t have an account?</p>
+                    <NavLink to={'/register'}>
+                        <p>Sign Up</p>
+                    </NavLink>
                 </div>
-                <div>
-                    <a>Sign Up</a>
-                </div>
+
 
             </form>
 
