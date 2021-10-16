37bcf1ca1cd3367217bfb671e28cad45de2ba096
It-Shu <it.shustov@gmail.com>
2021-10-16 17:14:08 +0300

    Login form Routs

--- src/pages/Login/Login.tsx
+++ src/pages/Login/Login.tsx
@@ -3,6 +3,7 @@ import {Input} from "../../components/UI/Input/Input";
 import {Button} from "../../components/UI/Button/Button";
 import {Checkbox} from "../../components/UI/Checkbox/Checkbox";
 import {NavLink} from "react-router-dom";
+import {PATH} from '../../routes/routes';
 
 
 export const Login: FC = () => {
@@ -20,7 +21,7 @@ export const Login: FC = () => {
                 <label htmlFor="loginPassword">Password</label>
                 <Input id={'loginPassword'} type={"password"}/>
 
-                <NavLink to={'/password-recovery'}>
+                <NavLink to={PATH.PASSWORD_RECOVERY}>
                     <p>Forgot Password</p>
                 </NavLink>
 
@@ -30,14 +31,11 @@ export const Login: FC = () => {
 
                 <div>
                     <p>Don’t have an account?</p>
-                    <NavLink to={'/register'}>
+                    <NavLink to={PATH.REGISTER}>
                         <p>Sign Up</p>
                     </NavLink>
                 </div>
-
-
             </form>
-
         </div>
     )
 }
