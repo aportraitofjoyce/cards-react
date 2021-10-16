efec8b88745a425b0f52257e93a1293e381627ca
aportraitofjoyce <aportraitofjoyce@gmail.com>
2021-10-16 14:07:24 +0300

    Structure changed

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,15 +1,17 @@
-import React from 'react'
+import React, {FC} from 'react'
 import {AppRouter} from './AppRouter'
 import {HashRouter} from 'react-router-dom'
 import {Header} from '../components/Header/Header'
 import {Provider} from 'react-redux'
 import {store} from '../store/store'
 
-export const App = () => (
-    <HashRouter>
-        <Provider store={store}>
-            <Header/>
-            <AppRouter/>
-        </Provider>
-    </HashRouter>
-)
+export const App: FC = () => {
+    return (
+        <HashRouter>
+            <Provider store={store}>
+                <Header/>
+                <AppRouter/>
+            </Provider>
+        </HashRouter>
+    )
+}
