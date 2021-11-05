1586d94e71701b0caf5ca70de65dfff9fb8e202c
aportraitofjoyce <aportraitofjoyce@gmail.com>
2021-11-05 13:13:11 +0300

    Some styles fix, content to homepage.

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -2,8 +2,11 @@ import React, {FC} from 'react'
 
 export const Home: FC = () => {
     return (
-        <div>
-            <h1>Home Page</h1>
+        <div style={{textAlign: 'center'}}>
+            <h1>Learning cards</h1>
+            <h2>Here you can create your own card packs with questions and answers.</h2>
+            <h2>Then you can check your skills trying to give give answers to cards and grading your success or failure.</h2>
+            <h2>Future cards will be offered based on personal grade.</h2>
         </div>
     )
 }
